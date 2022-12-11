468621a06ca92871aec3243800174206f171ebc9
Krzysztof Gronski <krzysztof.gronski.pl@gmail.com>
2022-12-11 20:18:17 +0100

    custom hook useTasks

--- src/App.js
+++ src/App.js
@@ -1,65 +1,29 @@
-
 import React, { useEffect, useState, useRef } from "react";
 import Section from "./Section";
 import Form from "./Form";
 import TasksList from "./TasksList";
 import Header from "./Header";
 import Container from "./Container";
+import {useTasks} from "./useTasks.js";
 
 function App() {
     const [hideDone, setHideDone] = useState(JSON.parse(localStorage.getItem("hideDone")) || false);
-    const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
     const inputRef = useRef(null);
-
-    useEffect(() => {
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-        inputRef.current.focus();
-    }, [tasks]);
-
-    useEffect(() => {
-        localStorage.setItem("hideDone", JSON.stringify(hideDone));
-        inputRef.current.focus();
-    }, [hideDone]);
 
     const toggleHideDone = () => {
         setHideDone(hideDone => !hideDone);
         inputRef.current.focus();
     };
 
-    const removeTask = (id) => {
-        setTasks(tasks => tasks.filter(
-            task => task.id !== id
-        ));
-    };
+    const {tasks, removeTask, toggleTaskDone, setAllDone, addNewTask}=useTasks(hideDone);
 
-    const toggleTaskDone = (id) => {
-        setTasks(tasks => tasks.map(task => {
-            if (task.id === id) {
-                return {
-                    ...task, done: !task.done
-                };
-            };
-            return task;
-        }));
-    };
+    useEffect(() => {
+        inputRef.current.focus();
+    }, [hideDone]);
 
-    const setAllDone = () => {
-        setTasks(tasks => tasks.map(task => ({
-            ...task,
-            done: true,
-        })))
-    };
-
-    const addNewTask = (content) => {
-        setTasks(tasks => [
-            ...tasks,
-            {
-                content,
-                done: false,
-                id: tasks.length !== 0 ? tasks[tasks.length - 1].id + 1 : 1,
-            },
-        ]);
-    };
+    useEffect(() => {
+        inputRef.current.focus();
+    }, [tasks]);
 
     return (
 
