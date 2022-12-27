3e4c2dfab605c8028e2beceea153a961e667d9df
Krzysztof Gronski <krzysztof.gronski.pl@gmail.com>
2022-12-27 12:36:03 +0100

    cleaning jobs

--- src/features/tasks/Header/index.js
+++ src/features/tasks/Header/index.js
@@ -1,27 +1,42 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectTasks, toggleHideDone, setAllDone } from "../tasksSlice";
-import { SectionHeader, ListHeader, ButtonsHeader, ListHeaderButton } from "./styled";
+import {
+  SectionHeader,
+  ListHeader,
+  ButtonsHeader,
+  ListHeaderButton,
+} from "./styled";
 
 const Header = ({ headerTitle, body }) => {
-    let headerButtons;
-    const { tasks, hideDone } = useSelector(selectTasks);
-    const dispatch = useDispatch();
+  let headerButtons;
+  const { tasks, hideDone } = useSelector(selectTasks);
+  const dispatch = useDispatch();
 
-    if (body) {
-        if (tasks.length) {
-            headerButtons =
-                <React.Fragment>
-                    <ListHeaderButton onClick={() => dispatch(toggleHideDone())}>{hideDone ? "Pokaż ukończone" : "Ukryj ukończone"}</ListHeaderButton>
-                    <ListHeaderButton disabled={tasks.every(({ done }) => done)} onClick={() => dispatch(setAllDone())}>Ukończ wszystkie</ListHeaderButton>
-                </React.Fragment>;
-        }
-        return <SectionHeader>
-            <ListHeader>{headerTitle}</ListHeader>
-            <ButtonsHeader>{headerButtons}</ButtonsHeader>
-        </SectionHeader>;
+  if (body) {
+    if (tasks.length) {
+      headerButtons = (
+        <React.Fragment>
+          <ListHeaderButton onClick={() => dispatch(toggleHideDone())}>
+            {hideDone ? "Pokaż ukończone" : "Ukryj ukończone"}
+          </ListHeaderButton>
+          <ListHeaderButton
+            disabled={tasks.every(({ done }) => done)}
+            onClick={() => dispatch(setAllDone())}
+          >
+            Ukończ wszystkie
+          </ListHeaderButton>
+        </React.Fragment>
+      );
     }
-    return <SectionHeader>{headerTitle}</SectionHeader>;
+    return (
+      <SectionHeader>
+        <ListHeader>{headerTitle}</ListHeader>
+        <ButtonsHeader>{headerButtons}</ButtonsHeader>
+      </SectionHeader>
+    );
+  }
+  return <SectionHeader>{headerTitle}</SectionHeader>;
 };
 
 export default Header;
--- src/features/tasks/Header/styled.js
+++ src/features/tasks/Header/styled.js
@@ -1,54 +1,50 @@
 import styled from "styled-components";
 
 export const SectionHeader = styled.header`
-    display: grid;
-    grid-gap: 20px;
-    grid-template-columns: auto auto;
-    justify-content: space-between;
-    align-items: center;
-    margin: 0;
-    border-bottom: 1px solid #ddd;
-    padding: 20px;
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: auto auto;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0;
+  border-bottom: 1px solid #ddd;
+  padding: 20px;
 
-    @media (max-width: 767px) {
-        grid-template-columns: auto;
-        justify-content: center;
-    }
+  @media (max-width: 767px) {
+    grid-template-columns: auto;
+    justify-content: center;
+  }
 `;
 
 export const ListHeader = styled.h2`
-    margin-block-end: 0;
-    margin-block-start: 0;
+  margin-block-end: 0;
+  margin-block-start: 0;
 `;
 
 export const ButtonsHeader = styled.div`
-    display: grid;
-    grid-template-columns: auto auto;
-    grid-gap: 30px;
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-gap: 30px;
 
-@media (max-width: ${({theme})=>theme.breakpoints.mobile}px) {
-        grid-template-columns: auto;
-        justify-content: center;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    grid-template-columns: auto;
+    justify-content: center;
+  }
 `;
 
 export const ListHeaderButton = styled.button`
-    transition: 1s;
-    color: ${({theme})=>theme.colors.primary};
-    border: none;
-    background: rgba(255, 255, 255, 0);
-    padding: 0;
+  transition: 1s;
+  color: ${({ theme }) => theme.colors.primary};
+  border: none;
+  background: rgba(255, 255, 255, 0);
+  padding: 0;
 
-    &:hover{
-        color: ${({theme})=>theme.colors.primaryHover};
-        cursor: pointer;
-    }
-    &:disabled{
-        color: gray;
-        cursor: not-allowed;
-    }
-
+  &:hover {
+    color: ${({ theme }) => theme.colors.primaryHover};
+    cursor: pointer;
+  }
+  &:disabled {
+    color: gray;
+    cursor: not-allowed;
+  }
 `;
-
-
-
