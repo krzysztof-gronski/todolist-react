5ac6372ad5f74449c406396cb8e7d6e32039063f
Krzysztof Gronski <krzysztof.gronski.pl@gmail.com>
2023-01-13 11:52:53 +0100

    search hooks usage

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,23 +1,21 @@
 import React from "react";
-import { useHistory, useLocation } from "react-router-dom";
 import Container from "./styled";
 import Input from "../../Input";
 import searchQueryParamName from "../searchQueryParamName";
+import {
+  useQueryParameter,
+  useReplaceQueryParameter,
+} from "../queryParameters";
 
 const Search = () => {
-  const location = useLocation();
-  const history = useHistory();
-  const query = new URLSearchParams(location.search).get(searchQueryParamName);
+  const query = useQueryParameter(searchQueryParamName);
+  const replaceQueryParameter = useReplaceQueryParameter();
 
   const onInputChange = ({ target }) => {
-    const searchParams = new URLSearchParams(location.search);
-
-    if (target.value.trim() === "") {
-      searchParams.delete(searchQueryParamName);
-    } else {
-      searchParams.set(searchQueryParamName, target.value);
-    }
-    history.push(`${location.pathname}?${searchParams.toString()}`);
+    replaceQueryParameter({
+      key: searchQueryParamName,
+      value: target.value.trim() !== "" ? target.value : undefined,
+    });
   };
 
   return (
