10ceb3125361c477af0cc727eefdc68466bd9480
Krzysztof Gronski <krzysztof.gronski.pl@gmail.com>
2023-01-14 20:11:48 +0100

    json url update2

--- src/features/tasks/api.js
+++ src/features/tasks/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getSampleTasks = async () => {
-  const response = await axios.get("%PUBLIC_URL%/sampleTasks.json");
+  const response = await axios.get("/todolist-react/sampleTasks.json");
   if (response.statusText !== "OK" || typeof response.data !== "object") {
     new Error(response.statusText);
     return false;
