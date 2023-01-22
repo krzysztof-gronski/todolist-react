8d53364f8756062f43871c0daf6694eeacc7f0d3
Krzysztof Gronski <krzysztof.gronski.pl@gmail.com>
2023-01-22 20:14:38 +0100

    debugging - axios.get modified

--- src/features/tasks/api.js
+++ src/features/tasks/api.js
@@ -19,10 +19,17 @@ export const getSampleTasks = async () => {
 //   }
 // ];
 
+  // const response = await axios.get("/todolist-react/sampleTasks.json");
+  // if (response.statusText !== "OK" || typeof response.data !== "object") {
+  //   new Error(response.statusText);
+  //   return false;
+  // }
+  // return response.data;
+
   const response = await axios.get("/todolist-react/sampleTasks.json");
-  if (response.statusText !== "OK" || typeof response.data !== "object") {
+  if (response.statusText !== "OK") {
     new Error(response.statusText);
-    return false;
   }
   return response.data;
+
 };
