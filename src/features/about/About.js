f9b7b6e861cefc6f7b91fdd8fce92efe1ebd5e5d
Krzysztof Gronski <krzysztof.gronski.pl@gmail.com>
2023-01-24 10:48:22 +0100

    cleaning jobs

--- src/features/about/About.js
+++ src/features/about/About.js
@@ -8,17 +8,31 @@ import { RegularLink } from "../../common/Link";
 function About() {
   return (
     <Container title="O autorze">
-      <Section header={<Header headerTitle="Krzysztof Groński" />}
-      body={<>
-        <Paragraph>Mgr inż. informatyki <Emoticon>👨‍🎓 </Emoticon> ukończone studia
-        magistersko-inżynierskie na
-        kierunku <strong>Inżynieria oprogramowania i systemy informatyczne</strong>, certyfikowany kurs frontend developera w <RegularLink href="https://youcode.pl"><strong>Youcode</strong></RegularLink>.
-        Pasjonat technik programistycznych z frameworkiem React (JS) na czele.</Paragraph> 
-        <Paragraph>Prywatnie miłośnik latynoskiej szkoły tańca w stylu salsy kubańskiej, kizomby i bachaty <Emoticon>🕺</Emoticon> 
-          Na codzień praktyk metod zdrowego odżywiania oraz ćwiczeń jogi w celu zachowania długotrwałego zdrowia
-          oraz utrzymania solidnej kondycji psycho-fizycznej.
-          </Paragraph></>
-      } />
+      <Section
+        header={<Header headerTitle="Krzysztof Groński" />}
+        body={
+          <>
+            <Paragraph>
+              Mgr inż. informatyki <Emoticon>👨‍🎓 </Emoticon> ukończone studia
+              magistersko-inżynierskie na kierunku{" "}
+              <strong>Inżynieria oprogramowania i systemy informatyczne</strong>
+              , certyfikowany kurs frontend developera w{" "}
+              <RegularLink href="https://youcode.pl">
+                <strong>Youcode</strong>
+              </RegularLink>
+              . Pasjonat technik programistycznych z frameworkiem React (JS) na
+              czele.
+            </Paragraph>
+            <Paragraph>
+              Prywatnie miłośnik latynoskiej szkoły tańca w stylu salsy
+              kubańskiej, kizomby i bachaty <Emoticon>🕺</Emoticon>
+              Na codzień praktyk metod zdrowego odżywiania oraz ćwiczeń jogi w
+              celu zachowania długotrwałego zdrowia oraz utrzymania solidnej
+              kondycji psycho-fizycznej.
+            </Paragraph>
+          </>
+        }
+      />
     </Container>
   );
 }
